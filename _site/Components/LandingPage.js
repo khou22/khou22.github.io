4958c170d05c94e95d392eb8ade7e098bd8a8d7b
Kevin Hou <kevin.ch.hou@gmail.com>
2015-08-11 00:21:21 -0700

    Added background image to landing page.

--- _site/Components/LandingPage.js
+++ _site/Components/LandingPage.js
@@ -1,14 +1,25 @@
 var LandingPage = React.createClass({
   render: function() {
     return (
-      <div className="landing-background">
-        Click <a href="/blog">here</a> for my blog
-        <img src="./media/blog/images/Woodcraft081015.jpg"></img>
-        <img src="./media/site/images/Profile.jpg"></img>
+      <div>
+        <div className="content-area">
+          <ProfileBox />
+          Click <a href="/blog">here</a> for my blog
+        </div>
+        <div className="landing-background"></div>
       </div>
     );
   }
 });
+
+var ProfileBox = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+});
+
 React.render(
   React.createElement(LandingPage, null),
   document.getElementById('content')
