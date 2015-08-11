0f656cd987d8721ac49d444faee780bf35a2aa60
Kevin Hou <kevin.ch.hou@gmail.com>
2015-08-11 00:51:22 -0700

    Added profile picture. Cropped background image.

--- _site/Components/LandingPage.js
+++ _site/Components/LandingPage.js
@@ -4,7 +4,7 @@ var LandingPage = React.createClass({
       <div>
         <div className="content-area">
           <ProfileBox />
-          Click <a href="/blog">here</a> for my blog
+          <ModalBox showModal={false} />
         </div>
         <div className="landing-background"></div>
       </div>
@@ -15,10 +15,44 @@ var LandingPage = React.createClass({
 var ProfileBox = React.createClass({
   render: function() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <div className="profile-name">
+          Kevin Hou
+        </div>
+        <div className="profile-picture"></div>
+        Click <a href="/blog">here</a> for my blog
+      </div>
     );
   }
 });
+
+var ModalBox = React.createClass({
+  render: function() {
+    if (this.props.showModal) {
+      return (
+        <div aria-hidden="false" role="dialog" className="modal">
+          <div className="modal__container">
+            <div className="modal__header">
+              Header
+            </div>
+            <div className="modal__content">
+              Content
+            </div>
+            <div className="modal__footer">
+              Footer
+            </div>
+          </div>
+          <div className="modal-backdrop">
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <span></span>
+      )
+    }
+  }
+})
 
 React.render(
   React.createElement(LandingPage, null),
