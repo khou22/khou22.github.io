caab726e71ec3bb2235c7754b890d990acf2da3a
Kevin Hou <kevin.ch.hou@gmail.com>
2015-08-11 14:08:08 -0700

    Added slideshow in background. Preloads images.

--- _site/Components/LandingPage.js
+++ _site/Components/LandingPage.js
@@ -1,23 +1,44 @@
 //Landing page for my website
 
+var images = new Array() //Declare array
+function preload() { //Preload the images ahead of time so they don't lag
+  for (i = 0; i < preload.arguments.length; i++) {
+    images[i] = new Image()
+    images[i].src = preload.arguments[i]
+  }
+}
+
 var LandingPage = React.createClass({
   getInitialState: function() {
+    var backgrounds = [
+      "./../media/site/images/backgrounds/Walnut_Poplar_Salad_Bowl.jpg",
+      "./../media/site/images/backgrounds/Basket_Weave_Cutting_Board.jpg"
+    ];
+    setInterval(this.nextBackground.bind(this), 5000);
+    for (var i = 0; i < backgrounds.length; i++) { //Cycle through backgrounds
+      preload(backgrounds[i]); //Preload every background
+    }
     return {
-      backgroundURL: [
-        "./../media/site/images/backgrounds/Walnut_Poplar_Salad_Bowl.jpg", //Background
-      ],
+      backgroundURLs: backgrounds,
       currentBackground: 0 //Index in array of backgrounds
     };
   },
   nextBackground: function() {
-    this.setState({
-      currentBackground: this.state.currentBackground++ //Go to next background index
-    })
+    if (this.state.currentBackground < this.state.backgroundURLs.length - 1) { //if not the last background in array
+      this.setState({
+        currentBackground: this.state.currentBackground + 1 //Go to next background index
+      });
+    } else {
+      console.log("Reseting counter")
+      this.setState({
+        currentBackground: 0 //Reset index
+      })
+    }
   },
   render: function() {
-    var URL = this.state.backgroundURL[this.state.currentBackground];
+    var URL = this.state.backgroundURLs[this.state.currentBackground];
     var backgroundStyle = {
-      backgroundImage: 'url(' + URL + ')'
+      backgroundImage: 'url(' + URL + ')',
     };
     return (
       <div>
