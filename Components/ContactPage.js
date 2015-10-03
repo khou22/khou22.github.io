7a75ab6cc4635389a0c5dac434bec3eb204aa392
Kevin Hou <kevin.ch.hou@gmail.com>
2015-10-03 14:18:37 -0700

    Functioning contact page

--- Components/ContactPage.js
+++ Components/ContactPage.js
@@ -1,8 +1,38 @@
 var ContactPage = React.createClass({
   render: function() {
-    console.log("Working")
     return (
-      <h1>Hi</h1>
+      <div>
+        <h1 className="contact-header">Contact Me</h1>
+        <h3>Got a project idea? Suggestion? Feedback? Let me know</h3>
+        <br />
+        <div className="contact-main-column">
+          <ContactInfo />
+        </div>
+        <div className="contact-profile-column">
+          <ProfilePicture />
+        </div>
+      </div>
+    );
+  }
+})
+
+var ContactInfo = React.createClass({
+  render: function() {
+    return (
+      <span>
+        <h4>Email me at: <a href="mailto:kevin.ch.hou@gmail.com">kevin.ch.hou@gmail.com</a></h4>
+        <h4>Message me on <a href="https://www.linkedin.com/in/kevinhou22">LinkedIn</a></h4>
+      </span>
+    );
+  }
+})
+
+var ProfilePicture = React.createClass({
+  render: function() {
+    return (
+      <span>
+        <img src="../../media/site/images/Profile.jpg" />
+      </span>
     );
   }
 })
