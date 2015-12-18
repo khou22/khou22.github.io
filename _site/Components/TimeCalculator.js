2cd79ec05390f646e99c6a031cee1c3e0f4c37fe
Kevin Hou <kevin.ch.hou@gmail.com>
2015-12-17 20:20:16 -0800

    Started working on the time calculator app. Added the scss file

--- _site/Components/TimeCalculator.js
+++ _site/Components/TimeCalculator.js
@@ -4,10 +4,34 @@ var App = React.createClass({
       <div>
         Time Calculator
         <p>This is a work in progress. Started: December 16, 2015 19:15 PST</p>
+        <InputField />
       </div>
     );
   }
 });
+
+var InputField = React.createClass({
+  render: function() {
+    return (
+      <div className="tcalc-input-div">
+        <form>
+          <TimeInput />
+        </form>
+      </div>
+    );
+  }
+});
+
+var TimeInput = React.createClass({
+  render: function() {
+    return (
+      <span>
+        <input type="number" className="tcalc-hour-input" />:
+        <input type="number" className="tcalc-minutes-input" />
+      </span>
+    );
+  }
+})
 
 React.render(
   React.createElement(App, null),
