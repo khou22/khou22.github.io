8fd28f4a731bceb68b97582df0ddca7773cba3c9
Kevin Hou <kevin.ch.hou@gmail.com>
2016-04-09 14:44:58 -0700

    List reordering works. Can move items up the list now.

--- Components/DragDropInterface.js
+++ Components/DragDropInterface.js
@@ -3,33 +3,83 @@ var App = React.createClass({ //Main parent component
     return {
       list: [
         {
-          label: "x",
+          label: "0x",
           color: "red",
           order: 0
         },
         {
-          label: "3x",
+          label: "1x",
           color: "blue",
-          order: 3
+          order: 1
         },
         {
-          label: "4x",
+          label: "2x",
           color: "purple",
           order: 2
         },
         {
-          label: "2x",
+          label: "3x",
           color: "green",
-          order: 1
+          order: 3
         },
-      ]
+        {
+          label: "4x",
+          color: "green",
+          order: 4
+        },
+        {
+          label: "5x",
+          color: "green",
+          order: 5
+        },
+        {
+          label: "6x",
+          color: "green",
+          order: 6
+        },
+      ],
+      currentDragIndex: 0,
+      currentDropLocation: 0
     }
   },
   setDropIndex: function(index) { // Store index you're dropping over
+    // Fire when dropping an object
     console.log("Dropping object over", index); // Feedback
+    this.setState({
+      currentDropLocation: index // Store
+    })
+
+    // Logic to reorder list
+    var originalIndex = this.state.currentDragIndex;
+    var desiredIndex = index;
+    console.log("Reordering list. Moving", originalIndex, "to", desiredIndex);
+    var list = this.state.list;
+    var updatedList = this.state.list;
+
+    for (var i = 0; i < list.length; i++) {
+      if (originalIndex < desiredIndex) { // If moving an object higher on the list
+        console.log("Checking", list[i].label);
+        if (list[i].order == originalIndex) {
+          console.log("Changing", list[i].label, "index to", desiredIndex)
+          updatedList[i].order = desiredIndex; // Change order of original object
+        } else if (list[i].order > originalIndex && list[i].order <= desiredIndex) {// If between the two
+          var newIndex = updatedList[i].order - 1;
+          console.log("Changing", list[i].label, "index to", newIndex)
+          updatedList[i].order = newIndex;
+        }
+      }
+    }
+
+    console.log(updatedList);
+    this.setState({ // Update state
+      list: updatedList
+    })
   },
   setDragObject: function(index) { // Store index of what you're dropping
-    console.log("Dragging object", index)
+    console.log("Dragging object", index);
+    this.setState({
+      currentDragIndex: index // Store
+    })
   },
   render: function() {
     var data = this.state.list;
