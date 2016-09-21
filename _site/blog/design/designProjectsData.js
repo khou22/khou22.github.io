2a9ea8f0fbe3a820036819ec35012f5e4263c3c0
Kevin Hou <kevin.ch.hou@gmail.com>
2016-09-21 10:41:21 -0400

    Removed projects from the design portfolio page

--- _site/blog/design/designProjectsData.js
+++ _site/blog/design/designProjectsData.js
@@ -7,24 +7,6 @@ programmingProjects = [
     description: "An iOS app to call Ubers ahead of time or on a recurring schedule. I was the frontend engineer and designer. We built this at a hackathon and won 1st place overall."
   },
   {
-    name: "Execute Order 66",
-    slug: "Execute-Order-66",
-    image: "",
-    description: "A Google Chrome Extension that cleanses the profanity out of your GitHub repos."
-  },
-  {
-    name: "Text Analysis",
-    slug: "text-analysis",
-    image: "",
-    description: "An app to analyze the user's text for patterns, word choice, etc. It accepts text files as well as Facebook user data."
-  },
-  {
-    name: "Drag and Drop Interface",
-    slug: "drag-drop-interface",
-    image: "",
-    description: "A simple drag and drop interface that uses ReactJS and HTML5 to mimic the Desmos equation editor."
-  },
-  {
     name: "City Strong Training",
     slug: "http://citystrongtraining.com/",
     image: "",
@@ -35,30 +17,6 @@ programmingProjects = [
     slug: "{{ site.baseurl }}/school/east-asian-relations",
     image: "",
     description: "This is a website I built from scratch for my East Asian Relations class. I was responsible for putting content online so that members of our school community could see the work we'd done that year. Because I only had a couple days to complete it, I wasn't able to add as many features as I would've liked."
-  },
-  {
-    name: "Stats Test Score Calculator",
-    slug: "stats-test-score-calculator",
-    image: "",
-    description: "An app to automatically calculate test scores and apply a grading curve. Built for Triggs Honors Stats Classes."
-  },
-  {
-    name: "Time Calculator",
-    slug: "time-calculator",
-    image: "",
-    description: "An app that takes in an ambiguous number of times and outputs the average of those times. Made this for my friend who does crew."
-  },
-  {
-    name: "Presidential Election Forecast 2016",
-    slug: "presidential-election-forecast-2016",
-    image: "",
-    description: "Runs simulations and forecasts who will win the 2016 presidential election. Right now it only simulates who will win the Republican candidacy."
-  },
-  {
-    name: "Browser Home Page",
-    slug: "browser-home-page",
-    image: "",
-    description: "Custom browser home page with useful information and links. I set it to my tab/browser home page."
   },
   {
     name: "Honors Physics Optics Lab",
@@ -77,11 +35,5 @@ programmingProjects = [
     slug: "{{ site.baseurl }}/blog/wallpapers/",
     image: "",
     description: "Some wallpapers I designed from the ground up. Mutliple versions for multiple different devices."
-  },
-  {
-    name: "Wood Club",
-    slug: "wood-club",
-    image: "",
-    description: "The birth and evolution of Wood Club - a club I lead at my high school"
   }
 ]
