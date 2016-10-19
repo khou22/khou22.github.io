7cecb45fd2467ee5d2fff0084bf1c0e790e7b1f4
Kevin Hou <kevin.ch.hou@gmail.com>
2016-10-19 17:21:06 -0400

    Updated image placeholder for programming projects

--- _site/blog/programming/programmingProjectsData.js
+++ _site/blog/programming/programmingProjectsData.js
@@ -3,97 +3,97 @@ programmingProjects = [
   {
     name: "Uber Scheduler",
     slug: "Uber-Scheduler",
-    image: "",
+    image: "#",
     description: "An iOS app to call Ubers ahead of time or on a recurring schedule. I was the frontend engineer and designer. We built this at a hackathon and won 1st place overall."
   },
   {
     name: "City Strong Training",
     slug: "http://citystrongtraining.com/",
-    image: "",
+    image: "#",
     description: "Freelance work for a friend of mine. I designed and built a website for his new personal training business, City Strong Training."
   },
   {
     name: "Website on East Asian Stereotypes",
     slug: "{{ site.baseurl }}/school/east-asian-relations",
-    image: "",
+    image: "#",
     description: "This is a website I built from scratch for my East Asian Relations class. I was responsible for putting content online so that members of our school community could see the work we'd done that year. Because I only had a couple days to complete it, I wasn't able to add as many features as I would've liked."
   },
   {
     name: "Handwriting Font App",
     slug: "https://www.youtube.com/watch?v=9qK6qmGWyB4",
-    image: "",
+    image: "#",
     description: "This is a handwriting app that I programmed using java. It asks for the user's handwriting then creates a font out of it. The user can type in their handwriting and export their document to be written out using the WaterColor Bot."
   },
   {
     name: "MusicViz - Live Music Visualizer",
     slug: "http://youtu.be/sbJhaRZhsZo",
-    image: "",
+    image: "#",
     description: "This is a music visualizer I programmed in Processing. You can input any music file and it will analyze the sound for beat drops, etc."
   },
   {
     name: "Online Sandwich Ordering Machine",
     slug: "https://www.flickr.com/photos/khou22/albums/72157668990077812",
-    image: "",
+    image: "#",
     description: "This is a project I made for my school. I used an Arduino Ethernet board and a receipt printer to make a machine that could process online sandwich orders for the school cafeteria."
   },
   {
     name: "Drag and Drop Interface",
     slug: "drag-drop-interface",
-    image: "",
+    image: "#",
     description: "A simple drag and drop interface that uses ReactJS and HTML5 to mimic the Desmos equation editor."
   },
   {
     name: "Stats Test Score Calculator",
     slug: "stats-test-score-calculator",
-    image: "",
+    image: "#",
     description: "An app to automatically calculate test scores and apply a grading curve. Built for Triggs Honors Stats Classes."
   },
   {
     name: "Time Calculator",
     slug: "time-calculator",
-    image: "",
+    image: "#",
     description: "An app that takes in an ambiguous number of times and outputs the average of those times. Made this for my friend who does crew."
   },
   {
     name: "Text Analysis",
     slug: "text-analysis",
-    image: "",
+    image: "#",
     description: "An app to analyze the user's text for patterns, word choice, etc. It accepts text files as well as Facebook user data."
   },
   {
     name: "Presidential Election Forecast 2016",
     slug: "presidential-election-forecast-2016",
-    image: "",
+    image: "#",
     description: "Runs simulations and forecasts who will win the 2016 presidential election. Right now it only simulates who will win the Republican candidacy."
   },
   {
     name: "Browser Home Page",
     slug: "browser-home-page",
-    image: "",
+    image: "#",
     description: "Custom browser home page with useful information and links. I set it to my tab/browser home page."
   },
   {
     name: "Honors Physics Optics Lab",
     slug: "{{ site.baseurl }}/school/optics/",
-    image: "",
+    image: "#",
     description: "A website I made from scratch for a physics lab on optics and waves. It was neat being able to make a website instead of a traditional website."
   },
   {
     name: "Hollywood's Portrayal of Africa",
     slug: "{{ site.baseurl }}/school/postcolonial-africa/",
-    image: "",
+    image: "#",
     description: "A website I made to highlight the stereotypes and microaggressions present in Hollywood films towards Africa. It was an interesting topic that isn't often thought about."
   },
   {
     name: "Self-Designed Wallpapers",
     slug: "{{ site.baseurl }}/blog/wallpapers/",
-    image: "",
+    image: "#",
     description: "Some wallpapers I designed from the ground up. Mutliple versions for multiple different devices."
   },
   {
     name: "Execute Order 66",
     slug: "https://chrome.google.com/webstore/detail/execute-order-66/hgoanjhaboccoaefceiebdodiillhone?hl=en-US",
-    image: "",
+    image: "#",
     description: "A Google Chrome Extension that cleanses the profanity out of your GitHub repos."
   }
 ]
