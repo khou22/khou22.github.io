653ec3a82c75073bba4ce75b389deebebb6fed85
Kevin Hou <kevin.ch.hou@gmail.com>
2016-11-17 11:06:59 -0500

    Built random number assigner. Assigns numbers for inputted elements.

--- _site/Components/RandomNumberAssignment.js
+++ _site/Components/RandomNumberAssignment.js
@@ -6,6 +6,38 @@ var RandomNumberAssignment = React.createClass({ // Main parent component
   },
   onSubmit: function() {
     var elementInput = document.getElementById('elements').value;
+
+    var elements = elementInput.split(',');
+
+    var numAssignments = []; // Track what's been assigned
+    for (var i = 0; i < elements.length; i++) {
+      numAssignments[i] = false; // Default not used
+    }
+
+    // Clean up array and give number
+    for (var i = 0; i < elements.length; i++) {
+      var chosen = false;
+      var number;
+      while (!chosen) {
+        // Random index for an array of length elements.length
+        var randomNumber = Math.round(Math.random() * (elements.length - 1));
+
+        if (!numAssignments[randomNumber]) { // If not used yet
+          number = randomNumber; // Store which value
+          numAssignments[randomNumber] = true; // Can't use anymore
+          chosen = true; // Leave while loop
+        }
+      }
+      elements[i] = {
+        item: elements[i].trim(), // Cut off white space
+        number: number + 1 // This way lowest number is 1
+      }
+    }
+
+    // Update state
+    this.setState({
+      elements: elements
+    })
   },
   render: function() {
     return (
@@ -13,9 +45,12 @@ var RandomNumberAssignment = React.createClass({ // Main parent component
         <h2>Random Number Assignment</h2>
         <p>Assigns element to a random order.</p>
         <br></br>
-        <input id="elements" onSubmit={this.onSubmit.bind(this)} />
+        <h4>Elements (Seperate by commas)</h4>
+        <textarea id="elements" className="rna-textarea-input"></textarea>
+        <br></br>
+        <button onClick={this.onSubmit.bind(this)}>Generate</button>
         <hr />
-        <Results />
+        <Results elements={this.state.elements}/>
       </div>
     );
   }
@@ -23,8 +58,16 @@ var RandomNumberAssignment = React.createClass({ // Main parent component
 
 var Results = React.createClass({
   render: function() {
+    var elementNodes = this.props.elements.map(function(element) {
+      return (
+        <div className="rna-element-node">
+          {element.item} - {element.number}
+        </div>
+      )
+    })
     return (
-      <div>
+      <div className="rna-element-results">
+        {elementNodes}
       </div>
     )
   }
