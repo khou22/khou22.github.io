1782ebca6a6e330b1d3304ea36b9d042694d6bdf
Kevin Hou <kevin.ch.hou@gmail.com>
2017-04-19 12:21:06 -0400

    Added tile layout for programming projects page

--- _site/blog/programming/programmingProjectsData.js
+++ _site/blog/programming/programmingProjectsData.js
@@ -1,117 +1,136 @@
-// console.log("Loaded programming projects");
+console.log("Loaded programming projects");
 programmingProjects = [
   {
     name: "SoFly Scanner",
+    subtitle: "",
     slug: "https://devpost.com/software/sofly-scanner",
-    image: "#",
+    image: "placeholder.jpg",
     description: "An app that allows the user to snap a picture of a flyer, extract relevant information, and make a calendar event with the event name, location, time, date, and more. This will allow users to digitize flyers, and hopefully provide a decentralized mechanism for communities to grow close again. It won 2nd place for best Princeton app at HackPrinceton Spring 2017."
   },
   {
     name: "Uber Scheduler",
+    subtitle: "",
     slug: "Uber-Scheduler",
-    image: "#",
+    image: "placeholder.jpg",
     description: "An iOS app to call Ubers ahead of time or on a recurring schedule. I was the frontend engineer and designer. We built this at a hackathon and won 1st place overall."
   },
   {
     name: "Princeton Tonight Website",
+    subtitle: "",
     slug: "http://princetontonight.com",
-    image: "",
+    image: "placeholder.jpg",
     description: "I designed and built Princeton Tonight's website to showcase episodes and upcoming events as well as an email subsription service. Founded in 2015, we are the only student TV production show on Princeton's campus and have hosted guests ranging from Art Garfunkel and Mike Houston to Mike Delguidice and Dave Coulier."
   },
   {
     name: "Website on East Asian Stereotypes",
+    subtitle: "",
     slug: "{{ site.baseurl }}/school/east-asian-relations",
-    image: "#",
+    image: "placeholder.jpg",
     description: "This is a website I built from scratch for my East Asian Relations class. I was responsible for putting content online so that members of our school community could see the work we'd done that year. Because I only had a couple days to complete it, I wasn't able to add as many features as I would've liked."
   },
   {
     name: "City Strong Training",
+    subtitle: "",
     slug: "http://citystrongtraining.com/",
-    image: "#",
+    image: "placeholder.jpg",
     description: "Freelance work for a friend of mine. I designed and built a website for his new personal training business, City Strong Training."
   },
   {
     name: "Handwriting Font App",
+    subtitle: "",
     slug: "https://www.youtube.com/watch?v=9qK6qmGWyB4",
-    image: "#",
+    image: "placeholder.jpg",
     description: "This is a handwriting app that I programmed using java. It asks for the user's handwriting then creates a font out of it. The user can type in their handwriting and export their document to be written out using the WaterColor Bot."
   },
   {
     name: "MusicViz - Live Music Visualizer",
+    subtitle: "",
     slug: "http://youtu.be/sbJhaRZhsZo",
-    image: "#",
+    image: "placeholder.jpg",
     description: "This is a music visualizer I programmed in Processing. You can input any music file and it will analyze the sound for beat drops, etc."
   },
   {
     name: "Online Sandwich Ordering Machine",
+    subtitle: "",
     slug: "https://www.flickr.com/photos/khou22/albums/72157668990077812",
-    image: "#",
+    image: "placeholder.jpg",
     description: "This is a project I made for my school. I used an Arduino Ethernet board and a receipt printer to make a machine that could process online sandwich orders for the school cafeteria."
   },
   {
     name: "Text Analysis",
+    subtitle: "",
     slug: "text-analysis",
-    image: "#",
+    image: "placeholder.jpg",
     description: "An app to analyze the user's text for patterns, word choice, etc. It accepts text files as well as Facebook user data."
   },
   {
     name: "Drag and Drop Interface",
+    subtitle: "",
     slug: "drag-drop-interface",
-    image: "#",
+    image: "placeholder.jpg",
     description: "A simple drag and drop interface that uses ReactJS and HTML5 to mimic the Desmos equation editor."
   },
   {
     name: "Stats Test Score Calculator",
+    subtitle: "",
     slug: "stats-test-score-calculator",
-    image: "#",
+    image: "placeholder.jpg",
     description: "An app to automatically calculate test scores and apply a grading curve. Built for Triggs Honors Stats Classes."
   },
   {
     name: "Time Calculator",
+    subtitle: "",
     slug: "time-calculator",
-    image: "#",
+    image: "placeholder.jpg",
     description: "An app that takes in an ambiguous number of times and outputs the average of those times. Made this for my friend who does crew."
   },
   {
     name: "Presidential Election Forecast 2016",
+    subtitle: "",
     slug: "presidential-election-forecast-2016",
-    image: "#",
+    image: "placeholder.jpg",
     description: "Runs simulations and forecasts who will win the 2016 presidential election. Right now it only simulates who will win the Republican candidacy."
   },
   {
     name: "Browser Home Page",
+    subtitle: "",
     slug: "browser-home-page",
-    image: "#",
+    image: "placeholder.jpg",
     description: "Custom browser home page with useful information and links. I set it to my tab/browser home page."
   },
   {
     name: "Honors Physics Optics Lab",
+    subtitle: "",
     slug: "{{ site.baseurl }}/school/optics/",
-    image: "#",
+    image: "placeholder.jpg",
     description: "A website I made from scratch for a physics lab on optics and waves. It was neat being able to make a website instead of a traditional website."
   },
   {
     name: "Hollywood's Portrayal of Africa",
+    subtitle: "",
     slug: "{{ site.baseurl }}/school/postcolonial-africa/",
-    image: "#",
+    image: "placeholder.jpg",
     description: "A website I made to highlight the stereotypes and microaggressions present in Hollywood films towards Africa. It was an interesting topic that isn't often thought about."
   },
   {
     name: "Self-Designed Wallpapers",
+    subtitle: "",
     slug: "{{ site.baseurl }}/blog/wallpapers/",
-    image: "#",
+    image: "placeholder.jpg",
     description: "Some wallpapers I designed from the ground up. Mutliple versions for multiple different devices."
   },
   {
     name: "Execute Order 66",
+    subtitle: "",
     slug: "https://chrome.google.com/webstore/detail/execute-order-66/hgoanjhaboccoaefceiebdodiillhone?hl=en-US",
-    image: "#",
+    image: "placeholder.jpg",
     description: "A Google Chrome Extension that cleanses the profanity out of your GitHub repos."
   },
   {
     name: "Random Number Assignment",
+    subtitle: "",
     slug: "random-number-assignment",
-    image: "#",
+    image: "placeholder.jpg",
     description: "Assigns a random number to inputted elements."
   }
 ]
---
+++ blog/programming/convertProjectsToGrid.js
@@ -0,0 +1,28 @@
+var gridData = {
+  columns: 4,
+  textColor: "white",
+  openNewWindow: true,
+  data: []
+}
+
+// Populate grid data from programmingProjects object
+for (var i = 0; i < programmingProjects.length; i++) {
+  var currentProject = programmingProjects[i];
+
+  var projectURL = currentProject.slug.replace("{{ site.baseurl }}", "http://khou22.github.io");
+
+  var gridItem = {
+    title: currentProject.name,
+    subtitle: currentProject.subtitle,
+    description: currentProject.description,
+    image: currentProject.image,
+    link: projectURL,
+    size: "2",
+    category: {
+      label: "Programming",
+      color: "orange"
+    }
+  }
+
+  gridData.data.push(gridItem); // Add to data
+}
