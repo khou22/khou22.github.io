e63b4648afcb95d8ddfc0d5eef8b164fd959eb19
Kevin Hou <kevin.ch.hou@gmail.com>
2017-05-06 13:20:09 -0400

    Clicking VSCO image links to the raw image

--- _site/photography/convertPhotosToGrid.js
+++ _site/photography/convertPhotosToGrid.js
@@ -8,13 +8,15 @@ var gridData = {
 // Populate grid data
 for (var i = 0; i < vscoImages.length; i++) {
   var currentImage = vscoImages[i];
+  var rawImageLink = "http://" + currentImage.substring(0, currentImage.length);
+  rawImageLink = rawImageLink.replace("?w=600", "?w=1200"); // Higher resolution
 
   var gridItem = {
     title: "",
     subtitle: "",
     description: "",
     image: "http://" + currentImage,
-    link: "http://vsco.co/khou22/",
+    link: rawImageLink,
     size: "1",
     category: {
       label: "",
