2a9ac3afc247d27539c87978faf3274c21a6c79a
Kevin Hou <kevin.ch.hou@gmail.com>
2017-05-09 19:19:20 -0400

    Working Heroku API endpoint

---
+++ Components/herokuAPITest.js
@@ -0,0 +1,39 @@
+var httpGetAsync = function(theUrl, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+var HerokuAPITest = React.createClass({
+	getInitialState: function() {
+		return {
+			random: 1
+		}
+	},
+  clickedButton: function() {
+    console.log("Calling Heroku API...");
+    var url = "https://guarded-lake-96979.herokuapp.com/test/2";
+    // var url = "http://localhost:5000/test/2";
+
+    httpGetAsync(url, function(response) {
+      console.log(response);
+    })
+  },
+	render: function() {
+		return (
+			<div>
+				<h1>Heroku API Test</h1>
+        <button onClick={this.clickedButton}>Test</button>
+			</div>
+		);
+	}
+})
+
+React.render(
+	React.createElement(HerokuAPITest, null),
+	document.getElementById('content')
+);
