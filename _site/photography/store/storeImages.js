1ea4ae43b19334e4fc44ff20bb2828ef19ccb4ae
Kevin Hou <kevin.ch.hou@gmail.com>
2018-11-27 01:08:06 -0500

    New photos for store

--- _site/photography/store/storeImages.js
+++ _site/photography/store/storeImages.js
@@ -1,155 +1,271 @@
-// Last updated: 2018-10-19 22:21:01
+// Last updated: 2018-11-27 00:46:36
 const storeImages = [
     {
-        name: "KHOU-0020-PRINT.jpg",
-        url: "../../media/store/KHOU-0020-PRINT.jpg",
+        name: "Times Square Rain Street.jpg",
+        url: "../../media/store/Times Square Rain Street.jpg",
     },
     {
-        name: "KHOU-0417-PRINT.jpg",
-        url: "../../media/store/KHOU-0417-PRINT.jpg",
+        name: "Lombard Street Drone.jpg",
+        url: "../../media/store/Lombard Street Drone.jpg",
     },
     {
-        name: "KHOU-0934-PRINT.jpg",
-        url: "../../media/store/KHOU-0934-PRINT.jpg",
+        name: "Larry Bao Two Man Boat Portrait.jpg",
+        url: "../../media/store/Larry Bao Two Man Boat Portrait.jpg",
     },
     {
-        name: "KHOU-0377-PRINT.jpg",
-        url: "../../media/store/KHOU-0377-PRINT.jpg",
+        name: "Mori Point Pacifica Long Exposure Rocks.jpg",
+        url: "../../media/store/Mori Point Pacifica Long Exposure Rocks.jpg",
+    },
+    {
+        name: "Fall Tree Lens Ball.jpg",
+        url: "../../media/store/Fall Tree Lens Ball.jpg",
+    },
+    {
+        name: "Grand Teton Landscape.jpg",
+        url: "../../media/store/Grand Teton Landscape.jpg",
+    },
+    {
+        name: "Roxelana Jewels Lamps.jpg",
+        url: "../../media/store/Roxelana Jewels Lamps.jpg",
+    },
+    {
+        name: "Bear Valley Astrophotography (Horizontal).jpg",
+        url: "../../media/store/Bear Valley Astrophotography (Horizontal).jpg",
+    },
+    {
+        name: "Minnesota Drone.jpg",
+        url: "../../media/store/Minnesota Drone.jpg",
+    },
+    {
+        name: "Bear Valley Astro Portrait.jpg",
+        url: "../../media/store/Bear Valley Astro Portrait.jpg",
     },
     {
         name: "Princeton Football Field At Night.jpg",
         url: "../../media/store/Princeton Football Field At Night.jpg",
     },
     {
-        name: "KHOU-0858-PRINT.jpg",
-        url: "../../media/store/KHOU-0858-PRINT.jpg",
+        name: "Santa Cruz Boardwalk Drone (Top Down).jpg",
+        url: "../../media/store/Santa Cruz Boardwalk Drone (Top Down).jpg",
     },
     {
-        name: "KHOU-0587-PRINT.jpg",
-        url: "../../media/store/KHOU-0587-PRINT.jpg",
+        name: "Times Square Santa.jpg",
+        url: "../../media/store/Times Square Santa.jpg",
     },
     {
-        name: "KHOU-0103-PRINT.jpg",
-        url: "../../media/store/KHOU-0103-PRINT.jpg",
+        name: "Artisan Dinner Table.jpg",
+        url: "../../media/store/Artisan Dinner Table.jpg",
     },
     {
-        name: "KHOU-0429-PRINT.jpg",
-        url: "../../media/store/KHOU-0429-PRINT.jpg",
+        name: "Times Square Taxi (Behind).jpg",
+        url: "../../media/store/Times Square Taxi (Behind).jpg",
     },
     {
-        name: "KHOU5-0319-PRINT.jpg",
-        url: "../../media/store/KHOU5-0319-PRINT.jpg",
+        name: "1903 Hall Drone Top Down.jpg",
+        url: "../../media/store/1903 Hall Drone Top Down.jpg",
     },
     {
-        name: "KHOU-0423-PRINT.jpg",
-        url: "../../media/store/KHOU-0423-PRINT.jpg",
+        name: "Times Square Taxi (Side).jpg",
+        url: "../../media/store/Times Square Taxi (Side).jpg",
     },
     {
-        name: "KHOU4-0488-PRINT.jpg",
-        url: "../../media/store/KHOU4-0488-PRINT.jpg",
+        name: "Golden Gate Bridge Night (Marin Headlands).jpg",
+        url: "../../media/store/Golden Gate Bridge Night (Marin Headlands).jpg",
     },
     {
-        name: "KHOU4-0394-PRINT.jpg",
-        url: "../../media/store/KHOU4-0394-PRINT.jpg",
+        name: "Bear Valley Astrophotography (Portrait).jpg",
+        url: "../../media/store/Bear Valley Astrophotography (Portrait).jpg",
     },
     {
-        name: "KHOU-0568-PRINT.jpg",
-        url: "../../media/store/KHOU-0568-PRINT.jpg",
+        name: "Loneliest Road Mountain.jpg",
+        url: "../../media/store/Loneliest Road Mountain.jpg",
     },
     {
-        name: "KHOU-0788-PRINT.jpg",
-        url: "../../media/store/KHOU-0788-PRINT.jpg",
+        name: "THS Tennis Court Drone (Drone Art).jpg",
+        url: "../../media/store/THS Tennis Court Drone (Drone Art).jpg",
     },
     {
-        name: "KHOU-0027-PRINT.jpg",
-        url: "../../media/store/KHOU-0027-PRINT.jpg",
+        name: "Cliff House Sutro Baths at Night.jpg",
+        url: "../../media/store/Cliff House Sutro Baths at Night.jpg",
     },
     {
-        name: "KHOU4-0593-PRINT.jpg",
-        url: "../../media/store/KHOU4-0593-PRINT.jpg",
+        name: "Loneliest Road Drone.jpg",
+        url: "../../media/store/Loneliest Road Drone.jpg",
     },
     {
-        name: "KHOU5-0001-PRINT.jpg",
-        url: "../../media/store/KHOU5-0001-PRINT.jpg",
+        name: "Clean Trahs Philly.jpg",
+        url: "../../media/store/Clean Trahs Philly.jpg",
     },
     {
-        name: "KHOU-0339-PRINT.jpg",
-        url: "../../media/store/KHOU-0339-PRINT.jpg",
+        name: "Loneliest Road (Vanishing Road).jpg",
+        url: "../../media/store/Loneliest Road (Vanishing Road).jpg",
     },
     {
-        name: "KHOU-0994-PRINT.jpg",
-        url: "../../media/store/KHOU-0994-PRINT.jpg",
+        name: "HanaHaus Coffee.jpg",
+        url: "../../media/store/HanaHaus Coffee.jpg",
     },
     {
-        name: "KHOU-0933-PRINT.jpg",
-        url: "../../media/store/KHOU-0933-PRINT.jpg",
+        name: "Delta Lake.jpg",
+        url: "../../media/store/Delta Lake.jpg",
     },
     {
-        name: "KHOU-0547-PRINT.jpg",
-        url: "../../media/store/KHOU-0547-PRINT.jpg",
+        name: "New York Street Artist (Side).jpg",
+        url: "../../media/store/New York Street Artist (Side).jpg",
     },
     {
-        name: "KHOU-0813-PRINT.jpg",
-        url: "../../media/store/KHOU-0813-PRINT.jpg",
+        name: "Jackson Hole Golf Course Drone.jpg",
+        url: "../../media/store/Jackson Hole Golf Course Drone.jpg",
     },
     {
-        name: "KHOU-0058-PRINT.jpg",
-        url: "../../media/store/KHOU-0058-PRINT.jpg",
+        name: "Burning Paper.jpg",
+        url: "../../media/store/Burning Paper.jpg",
+    },
+    {
+        name: "Jackson Golf Trevor Fishing.jpg",
+        url: "../../media/store/Jackson Golf Trevor Fishing.jpg",
+    },
+    {
+        name: "Patton Hall Drone.jpg",
+        url: "../../media/store/Patton Hall Drone.jpg",
+    },
+    {
+        name: "Top of the Rock Sign.jpg",
+        url: "../../media/store/Top of the Rock Sign.jpg",
+    },
+    {
+        name: "Time Square Taxi.jpg",
+        url: "../../media/store/Time Square Taxi.jpg",
+    },
+    {
+        name: "Grand Teton Astro with Grass.jpg",
+        url: "../../media/store/Grand Teton Astro with Grass.jpg",
+    },
+    {
+        name: "San Francisco from Russian Hill Drone.jpg",
+        url: "../../media/store/San Francisco from Russian Hill Drone.jpg",
+    },
+    {
+        name: "Christmas Ornament.jpg",
+        url: "../../media/store/Christmas Ornament.jpg",
+    },
+    {
+        name: "THS Tennis Court Drone (Top Down).jpg",
+        url: "../../media/store/THS Tennis Court Drone (Top Down).jpg",
+    },
+    {
+        name: "Grand Teton Landscape (2x3).jpg",
+        url: "../../media/store/Grand Teton Landscape (2x3).jpg",
+    },
+    {
+        name: "Loneliest Road Drone (Top Down).jpg",
+        url: "../../media/store/Loneliest Road Drone (Top Down).jpg",
+    },
+    {
+        name: "Fly Fishing Rod.jpg",
+        url: "../../media/store/Fly Fishing Rod.jpg",
     },
     {
         name: "Princeton Football Field (Down).jpg",
         url: "../../media/store/Princeton Football Field (Down).jpg",
     },
     {
-        name: "KHOU-0922-PRINT.jpg",
-        url: "../../media/store/KHOU-0922-PRINT.jpg",
+        name: "New York Street Artist (Front).jpg",
+        url: "../../media/store/New York Street Artist (Front).jpg",
     },
     {
-        name: "KHOU-0389-PRINT.jpg",
-        url: "../../media/store/KHOU-0389-PRINT.jpg",
+        name: "Times Square Lens Ball.jpg",
+        url: "../../media/store/Times Square Lens Ball.jpg",
     },
     {
-        name: "KHOU5--PRINT.jpg",
-        url: "../../media/store/KHOU5--PRINT.jpg",
+        name: "Dog Sledding Dog.jpg",
+        url: "../../media/store/Dog Sledding Dog.jpg",
     },
     {
-        name: "KHOU-0719-PRINT.jpg",
-        url: "../../media/store/KHOU-0719-PRINT.jpg",
+        name: "Piano Black and White.jpg",
+        url: "../../media/store/Piano Black and White.jpg",
     },
     {
-        name: "KHOU-0541-PRINT.jpg",
-        url: "../../media/store/KHOU-0541-PRINT.jpg",
+        name: "Larry Bao Two Man Boat Top Down.jpg",
+        url: "../../media/store/Larry Bao Two Man Boat Top Down.jpg",
     },
     {
-        name: "KHOU-0786-PRINT.jpg",
-        url: "../../media/store/KHOU-0786-PRINT.jpg",
+        name: "Lake Carnegie Landscape Fall Season.jpg",
+        url: "../../media/store/Lake Carnegie Landscape Fall Season.jpg",
     },
     {
-        name: "KHOU--PRINT.jpg",
-        url: "../../media/store/KHOU--PRINT.jpg",
+        name: "Delta Lake Glacier Runnoff.jpg",
+        url: "../../media/store/Delta Lake Glacier Runnoff.jpg",
     },
     {
-        name: "KHOU-0059-PRINT.jpg",
-        url: "../../media/store/KHOU-0059-PRINT.jpg",
+        name: "Mori Point Pacifica Sunset.jpg",
+        url: "../../media/store/Mori Point Pacifica Sunset.jpg",
     },
     {
-        name: "KHOU5-0763-PRINT.jpg",
-        url: "../../media/store/KHOU5-0763-PRINT.jpg",
+        name: "Yin Yang Golden Gate Bridge Light Painting.jpg",
+        url: "../../media/store/Yin Yang Golden Gate Bridge Light Painting.jpg",
     },
     {
-        name: "KHOU-0984-PRINT.jpg",
-        url: "../../media/store/KHOU-0984-PRINT.jpg",
+        name: "1903 Hall and Cuyler Hall Drone.jpg",
+        url: "../../media/store/1903 Hall and Cuyler Hall Drone.jpg",
     },
     {
-        name: "KHOU-0031-PRINT.jpg",
-        url: "../../media/store/KHOU-0031-PRINT.jpg",
+        name: "Princeton Boat House Fall Season.jpg",
+        url: "../../media/store/Princeton Boat House Fall Season.jpg",
     },
     {
-        name: "KHOU--PRINT-2.jpg",
-        url: "../../media/store/KHOU--PRINT-2.jpg",
+        name: "Grand Teton Bison.jpg",
+        url: "../../media/store/Grand Teton Bison.jpg",
     },
     {
-        name: "KHOU-0385-PRINT.jpg",
-        url: "../../media/store/KHOU-0385-PRINT.jpg",
+        name: "Teddy Niagara Falls.jpg",
+        url: "../../media/store/Teddy Niagara Falls.jpg",
+    },
+    {
+        name: "Devil's Tower.jpg",
+        url: "../../media/store/Devil's Tower.jpg",
+    },
+    {
+        name: "KHou Golden Gate Bridge Light Painting.jpg",
+        url: "../../media/store/KHou Golden Gate Bridge Light Painting.jpg",
+    },
+    {
+        name: "Badlands 1.jpg",
+        url: "../../media/store/Badlands 1.jpg",
+    },
+    {
+        name: "Rubicon Peak (Drone).jpg",
+        url: "../../media/store/Rubicon Peak (Drone).jpg",
+    },
+    {
+        name: "Palace of Fine Arts Street with Cars Drone.jpg",
+        url: "../../media/store/Palace of Fine Arts Street with Cars Drone.jpg",
+    },
+    {
+        name: "Times Square Screens.jpg",
+        url: "../../media/store/Times Square Screens.jpg",
+    },
+    {
+        name: "Mount Rushmore (Picture of iPhone).jpg",
+        url: "../../media/store/Mount Rushmore (Picture of iPhone).jpg",
+    },
+    {
+        name: "Badlands 2.jpg",
+        url: "../../media/store/Badlands 2.jpg",
+    },
+    {
+        name: "Grand Teton Morman Row.jpg",
+        url: "../../media/store/Grand Teton Morman Row.jpg",
+    },
+    {
+        name: "Times Square Long Exposure Cars.jpg",
+        url: "../../media/store/Times Square Long Exposure Cars.jpg",
+    },
+    {
+        name: "1903 Hall in Snow.jpg",
+        url: "../../media/store/1903 Hall in Snow.jpg",
+    },
+    {
+        name: "Sea Ranch Seals Drone (Top Down).jpg",
+        url: "../../media/store/Sea Ranch Seals Drone (Top Down).jpg",
     },
 ];
