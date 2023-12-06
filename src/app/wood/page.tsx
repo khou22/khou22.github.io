68b62959ced02a47c6b6cba29d94c32159fdc4f4
Kevin Hou <k@exafunction.com>
2023-12-06 10:51:53 -0800

    Add more copy to wood page

--- src/app/wood/page.tsx
+++ src/app/wood/page.tsx
@@ -5,7 +5,13 @@ import Image from "next/image";
 const WoodPortfolioPage = () => {
   return (
     <PageWrapper maxWidth="wide">
-      <h1 className="leading-loose">Wood</h1>
+      <div className="mb-8 w-full md:mb-12">
+        <h1 className="m-auto text-center leading-loose">Wood</h1>
+        <p className="m-auto max-w-lg text-center">
+          I&apos;ve been woodworking since I was in high school. Here are some
+          of my favorite projects.
+        </p>
+      </div>
       <div className="grid grid-cols-3 gap-x-2 gap-y-6">
         {woodProjects.map(({ title, link, image }) => (
           <div key={title} className="col-span-1">
