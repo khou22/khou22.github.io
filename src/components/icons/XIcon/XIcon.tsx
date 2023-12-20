7713d1ba15cd2b173a6a44fbbb14b78917c37ec0
Kevin Hou <k@exafunction.com>
2023-12-20 17:41:30 +0300

    Fix lint

--- src/components/icons/XIcon/XIcon.tsx
+++ src/components/icons/XIcon/XIcon.tsx
@@ -6,13 +6,13 @@ export const XIcon: React.FC<IconProps> = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke="currentColor"
     {...props}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M6 18L18 6M6 6l12 12"
     />
   </svg>
