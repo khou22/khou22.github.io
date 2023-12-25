0b3ce618c969627a8197f4b8ed7ac4d9b626bd25
Kevin Hou <kevin.ch.hou@gmail.com>
2023-12-25 14:42:05 -0800

    Add sendgrid contact form endpoint

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,48 @@
+import sgMail from "@sendgrid/mail";
+import moment from "moment";
+import { NextRequest } from "next/server";
+
+export type ContactFormRequest = {
+  full_name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+/**
+ * Contact form endpoint.
+ */
+export async function POST(request: NextRequest) {
+  try {
+    const body: ContactFormRequest = await request.json();
+
+    if (!process.env.SENDGRID_API_KEY) {
+      throw new Error("No Sendgrid API key");
+    }
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const sendgridPayload = {
+      to: process.env.CONTACT_FORM_ADDRESS,
+      from: body.email,
+      subject: body.subject,
+      text: `Name: ${body.full_name}
+Email: ${body.email}
+Subject: ${body.subject}
+Sent At: ${moment().format("llll")}
+
+${body.message}`,
+    };
+
+    await sgMail.send(sendgridPayload);
+
+    return new Response("Success", { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      return new Response(error.message, { status: 500 });
+    }
+    return new Response(`An unexpected error occurred: ${error}`, {
+      status: 500,
+    });
+  }
+}
