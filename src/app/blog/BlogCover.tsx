8cd6e20abdf5d124dfa1c86100beb34cbdb9ccf8
Kevin Hou <kevin.ch.hou@gmail.com>
2024-01-07 00:21:42 -0500

    Blog homepage is parallax

---
+++ src/app/blog/BlogCover.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { FadeInView } from "@/components/atoms/FadeInView/FadeInView";
+import { useIsClient } from "@/hooks/useIsClient/useIsClient";
+import Image from "next/image";
+import { useScreenSize } from "@/hooks/useScreenSize/useScreenSize";
+import { useScrollPosition } from "@/hooks/useScrollPosition/useScrollPosition";
+import { getCdnAsset } from "@/utils/cdn/cdnAssets";
+import { clamp } from "@/utils/math";
+import { classNames } from "@/utils/style";
+import { useRef } from "react";
+
+export const BlogCover: React.FC = () => {
+  const parallexContainer = useRef<HTMLDivElement>(null);
+  const { scrollY } = useScrollPosition();
+  const { height: screenHeight } = useScreenSize();
+  const isClient = useIsClient();
+  const isVisible = !isClient || scrollY <= screenHeight * 2;
+
+  // Only calculate the scroll when the parallax is visible to save on performance.
+  const scrollPageOffset = isVisible
+    ? clamp(0, screenHeight * 1.5, scrollY)
+    : 0;
+
+  return (
+    <div
+      className={classNames(
+        "relative block h-screen",
+        isVisible ? "opacity-100" : "opacity-0",
+      )}
+      ref={parallexContainer}
+    >
+      <Image
+        fill
+        src={getCdnAsset("media/site/images/Blog_Image_jpg")}
+        className="object-cover"
+        alt="Kevin Hou blog cover photo of basket weaving cutting boards"
+      />
+
+      <div
+        className="absolute left-0 top-[22%] z-10 flex w-full flex-col items-center justify-center"
+        style={{
+          transform: `translateY(${scrollPageOffset / 5}px)`,
+        }}
+      >
+        <FadeInView once>
+          <img
+            alt="Kevin Hou blog cover image"
+            src={getCdnAsset("media/site/images/Blog_Landing_Content_png")}
+            className="max-w-[500px] sm:max-w-[700px] md:max-w-[900px] 2xl:max-w-[1000px]"
+          />
+        </FadeInView>
+      </div>
+    </div>
+  );
+};
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata } from "next";
 import { getPosts } from "@/utils/blog/posts";
 import { PageWrapper } from "@/components/organisms/PageWrapper/PageWrapper";
-import Image from "next/image";
-import { getCdnAsset } from "@/utils/cdn/cdnAssets";
 import { PostList } from "@/components/organisms/PostList/PostList";
-import { FadeInView } from "@/components/atoms/FadeInView/FadeInView";
+import { BlogCover } from "./BlogCover";
 
 export const metadata: Metadata = {
   title: "Kevin Hou's Blog",
@@ -15,23 +13,7 @@ const BlogPage = () => {
 
   return (
     <>
-      <div className="relative h-screen w-screen">
-        <div className="absolute left-0 top-[22%] z-10 flex w-full flex-col items-center justify-center">
-          <FadeInView once>
-            <img
-              alt="Kevin Hou blog cover image"
-              src={getCdnAsset("media/site/images/Blog_Landing_Content_png")}
-              className="max-w-[500px] sm:max-w-[700px] md:max-w-[900px] 2xl:max-w-[1000px]"
-            />
-          </FadeInView>
-        </div>
-        <Image
-          fill
-          src={getCdnAsset("media/site/images/Blog_Image_jpg")}
-          className="object-cover"
-          alt="Kevin Hou blog cover photo of basket weaving cutting boards"
-        />
-      </div>
+      <BlogCover />
       <span className="h-0 w-full" id="posts" />
       <PageWrapper className="min-h-screen">
         <h2 className="leading-relaxed">Blog</h2>
