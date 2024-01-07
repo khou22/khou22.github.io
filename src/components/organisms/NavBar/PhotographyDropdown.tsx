071c5e9638a88030c5f5b82c58f8c1b0d620d2ce
Kevin Hou <kevin.ch.hou@gmail.com>
2024-01-07 18:17:50 -0500

    Add new tag to the nav bar

--- src/components/organisms/NavBar/PhotographyDropdown.tsx
+++ src/components/organisms/NavBar/PhotographyDropdown.tsx
@@ -1,5 +1,6 @@
 import { ImageCard } from "@/components/molecules/ImageCard/ImageCard";
 import { LinkWithArrow } from "@/components/molecules/LinkWithArrow/LinkWithArrow";
+import { Badge } from "@/components/ui/badge";
 import { PhotoTags } from "@/constants/photoTags";
 import { getCdnAsset } from "@/utils/cdn/cdnAssets";
 import { PAGES } from "@/utils/pages";
@@ -31,7 +32,14 @@ export const PhotographyDropdown: React.FC = () => {
         </li>
         <li className="border-b border-gray-400/50 pb-2">
           <LinkWithArrow href={PAGES.PHOTOGRAPHY.AERIAL_SF} className="text-sm">
-            San Francisco by Air
+            <div className="flex flex-row items-center justify-center space-x-2">
+              <span className="group-hover:text-blue-500">
+                San Francisco by Air
+              </span>
+              <div className="rounded-full bg-blue-500/75 px-2 py-0.5 text-xs font-bold text-white group-hover:bg-blue-500/90">
+                New
+              </div>
+            </div>
           </LinkWithArrow>
         </li>
         <li className="border-b border-gray-400/50 pb-2">
