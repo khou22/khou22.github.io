41ab65d9fb52ce8a1c7ed7bb9f202c4e168e42f3
Kevin Hou <kevin.ch.hou@gmail.com>
2024-02-14 10:24:57 -0800

    Context aware playground

---
+++ src/app/programming/codeium/ContextAwareDemo.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { CodeiumEditor, Document, Language } from "@codeium/react-code-editor";
+import { useState } from "react";
+
+const HTML_SNIPPET = `<html>
+  <head>
+    <title>Contact Form</title>
+  </head>
+  <body>
+    <h1>Contact Form</h1>
+    <p>I'm a simple contact form.</p>
+
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name-field-id" name="name" />
+
+      <label for="email">Email:</label>
+      <input type="email" id="email-field-id" name="email" />
+    </form>
+  </body>
+</html>
+`;
+
+const JAVSCRIPT_SNIPPET = `// You have context over a sample HTML page.
+// Codeium's generation will take this context into account when suggesting.
+
+// Get the contact form values by ID.`;
+
+export const ContextAwareDemo = () => {
+  const [htmlValue, setHtmlValue] = useState(HTML_SNIPPET);
+  const [javascriptValue, setJavascriptValue] = useState(JAVSCRIPT_SNIPPET);
+
+  return (
+    <div className="mt-4 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-8">
+      <div>
+        <CodeiumEditor
+          language="html"
+          value={htmlValue}
+          onChange={(value) => value && setHtmlValue(value)}
+          theme="vs-dark"
+          height={400}
+          otherDocuments={[
+            new Document({
+              absolutePath: "/script.js",
+              relativePath: "script.js",
+              text: javascriptValue,
+              editorLanguage: "javascript",
+              language: Language.JAVASCRIPT,
+            }),
+          ]}
+        />
+      </div>
+      <div>
+        <CodeiumEditor
+          language="javascript"
+          value={javascriptValue}
+          onChange={(value) => value && setJavascriptValue(value)}
+          theme="vs-dark"
+          height={400}
+          otherDocuments={[
+            new Document({
+              absolutePath: "/index.html",
+              relativePath: "index.html",
+              text: htmlValue,
+              editorLanguage: "html",
+              language: Language.HTML,
+            }),
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
--- src/app/programming/codeium/page.tsx
+++ src/app/programming/codeium/page.tsx
@@ -1,44 +1,71 @@
 import Link from "next/link";
 import { CodeiumEditor } from "@codeium/react-code-editor";
+import { ContextAwareDemo } from "./ContextAwareDemo";
 import { Button } from "@/components/ui/button";
 import { PageWrapper } from "@/components/organisms/PageWrapper/PageWrapper";
 
 const CodeiumDemoPage = () => {
   return (
-    <PageWrapper className="flex flex-col items-center justify-center space-y-4">
-      <div>
-        <h1 className="m-auto text-center leading-loose">Codeium</h1>
-        <p className="w-full text-center">Your modern coding superpower.</p>
-      </div>
+    <>
+      <PageWrapper className="flex flex-col items-center justify-center space-y-4">
+        <div>
+          <h1 className="m-auto text-center leading-loose">Codeium</h1>
+          <div className="mb-8 flex flex-row items-center justify-center space-x-2">
+            <Link href="https://codeium.com" target="_blank">
+              <Button variant="primary">Visit Codeium</Button>
+            </Link>
 
-      <div className="flex flex-row items-center justify-center space-x-2">
-        <Link href="https://codeium.com" target="_blank">
-          <Button variant="primary">Visit Codeium</Button>
-        </Link>
+            <Link
+              href="https://github.com/Exafunction/codeium-react-code-editor"
+              target="_blank"
+            >
+              <Button variant="outline">GitHub Repo</Button>
+            </Link>
+          </div>
+          <p className="mb-4 w-full text-center">
+            Codeium React Code Editor is a free, open-source code editor as a
+            React component with unlimited AI autocomplete. Brought to you by
+            the team at <Link href="https://codeium.com">Codeium</Link>. Free
+            with no account required. All you need to do is install our NPM
+            package, add it to your website and you&apos;re good to go!
+          </p>
+          <Link
+            href="https://github.com/Exafunction/codeium-react-code-editor"
+            target="_blank"
+          >
+            <img
+              src="https://badge.fury.io/js/@codeium%2Freact-code-editor.svg"
+              alt="Codeium badge"
+              className="m-auto h-5"
+            />
+          </Link>
+        </div>
 
-        <Link
-          href="https://github.com/Exafunction/codeium-react-code-editor"
-          target="_blank"
-        >
-          <Button variant="outline">GitHub Repo</Button>
-        </Link>
-      </div>
+        <div>
+          <h3 className="m-auto text-center">Basic Use Case</h3>
+          <p className="w-full text-center">
+            Try it out using the Python editor below.
+          </p>
+        </div>
 
-      <p>Try it out using the Python editor below.</p>
+        <CodeiumEditor language="python" theme="vs-dark" height={450} />
+      </PageWrapper>
 
-      <Link
-        href="https://github.com/Exafunction/codeium-react-code-editor"
-        target="_blank"
+      <PageWrapper
+        maxWidth="wide"
+        className="mt-6 flex flex-col items-center justify-center space-y-4"
       >
-        <img
-          src="https://badge.fury.io/js/@codeium%2Freact-code-editor.svg"
-          alt="Codeium badge"
-          className="h-5"
-        />
-      </Link>
+        <div>
+          <h3 className="m-auto text-center">Advanced Use Case</h3>
+          <p className="w-full text-center">
+            These two editors are context-aware. This means that they can use
+            each other&apos;s content as context when generating suggestions.
+          </p>
+        </div>
 
-      <CodeiumEditor language="python" theme="vs-dark" height={500} />
-    </PageWrapper>
+        <ContextAwareDemo />
+      </PageWrapper>
+    </>
   );
 };
 
