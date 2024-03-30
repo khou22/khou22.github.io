5a8186e1ec1f16056a9db634d6d1bea2199b856c
Kevin Hou <kevin.ch.hou@gmail.com>
2024-03-30 12:24:32 -0700

    New media page

---
+++ src/app/media/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { PageWrapper } from "@/components/organisms/PageWrapper/PageWrapper";
+import { getCdnAsset } from "@/utils/cdn/cdnAssets";
+import { DownloadLink } from "@/components/molecules/DownloadLink/DownloadLink";
+
+const MediaPage = () => {
+  return (
+    <PageWrapper className="flex flex-col items-center justify-center space-y-4">
+      <div className="mb-4 w-full border-b border-gray-300 pb-4 text-center">
+        <h1 className="leading-loose">Media</h1>
+        <p>Useful media resources both for my own use and for others!</p>
+      </div>
+      <div className="grid w-full grid-cols-3 gap-4">
+        <div className="my-12 space-y-1">
+          <Image
+            src={getCdnAsset("media/site/logo/initials_logo_svg")}
+            alt="Kevin Hou Logo"
+            width={100}
+            height={100}
+          />
+          <DownloadLink
+            fileName="kevin_hou_logo.svg"
+            url={getCdnAsset("media/site/logo/initials_logo_png")}
+          >
+            kevin_hou_logo.svg
+          </DownloadLink>
+          <DownloadLink
+            fileName="kevin_hou_logo"
+            url={getCdnAsset("media/site/logo/initials_logo_png")}
+          >
+            kevin_hou_logo.png
+          </DownloadLink>
+        </div>
+        <div className="my-12 space-y-1">
+          <Image
+            src={getCdnAsset("media/site/logo/initials_logo_animated_svg")}
+            alt="Kevin Hou Logo Animated"
+            width={100}
+            height={100}
+          />
+          <DownloadLink
+            fileName="kevin_hou_logo_animated.svg"
+            url={getCdnAsset("media/site/logo/initials_logo_animated_svg")}
+          >
+            kevin_hou_logo_animated.svg
+          </DownloadLink>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default MediaPage;
