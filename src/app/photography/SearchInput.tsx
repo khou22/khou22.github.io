31af3d962c508023cb8bccdda5403423d640247d
Kevin Hou <kevin.ch.hou@gmail.com>
2024-08-04 22:15:51 -0700

    Working search results page

--- src/app/photography/SearchInput.tsx
+++ src/app/photography/SearchInput.tsx
@@ -2,8 +2,13 @@
 
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { classNames } from "@/utils/style";
+import { ArrowRightIcon } from "@/components/icons/ArrowRightIcon/ArrowRightIcon";
 
-export const SearchInput = () => {
+type SearchInputProps = {
+  initialQuery?: string;
+};
+
+export const SearchInput: React.FC<SearchInputProps> = ({ initialQuery }) => {
   return (
     <div className="relative flex w-full flex-row items-center justify-center bg-gradient-to-b from-gray-100/10 to-gray-50 pb-4 pt-8">
       <form action="/photography/search">
@@ -17,10 +22,19 @@ export const SearchInput = () => {
           <MagnifyingGlassIcon className="h-6 w-6 shrink-0 opacity-100 transition-opacity duration-200 group-focus-within:opacity-0 group-hover:opacity-0" />
           <input
             placeholder="Search photos..."
+            defaultValue={initialQuery}
+            required
+            minLength={3}
             type="text"
             name="query"
             className="grow border-0 focus:ring-0 focus:ring-offset-0"
           />
+          <button
+            type="submit"
+            className="pointer-events-none cursor-pointer border-none bg-none group-focus-within:pointer-events-auto"
+          >
+            <ArrowRightIcon className="h-6 w-6 shrink-0 opacity-0 transition-opacity duration-200 group-focus-within:opacity-100" />
+          </button>
         </div>
       </form>
     </div>
--- src/app/photography/search/page.tsx
+++ src/app/photography/search/page.tsx
@@ -1,15 +1,52 @@
+import { SearchInput } from "../SearchInput";
+import { getPhotoIDs } from "@/utils/photos/getPhotoIDs";
 import { PageWrapper } from "@/components/organisms/PageWrapper/PageWrapper";
+import { getPhotoName } from "@/utils/cdn/cdnAssets";
+import { PhotoGallery } from "@/components/organisms/PhotoGallery/PhotoGallery";
+import { TagImageCard } from "@/components/organisms/TagImageCard/TagImageCard";
+import { PhotoTags } from "@/constants/photoTags/photoTags";
+import { tagMetadata } from "@/constants/photoTags/tagMetadata";
 
 const SearchPage = async ({
-  searchParams,
+  searchParams: { query },
 }: {
   params: {};
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: { query: string };
 }) => {
-  console.log(searchParams);
+  const matchingTags: PhotoTags[] = Object.values(PhotoTags).filter((tag) => {
+    return (
+      tag.toLowerCase().includes(query.toLowerCase()) ||
+      tagMetadata[tag].name.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+  const tagNodes = await Promise.all(
+    matchingTags.map(async (tag) => <TagImageCard key={tag} photoTag={tag} />),
+  );
+
+  const photoIDs = getPhotoIDs().filter((id) => {
+    return (
+      id.toLowerCase().includes(query.toLowerCase()) ||
+      getPhotoName(id).toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
   return (
     <PageWrapper maxWidth="wide">
-      <h1>Search</h1>
+      <SearchInput initialQuery={query} />
+
+      {tagNodes.length > 0 && (
+        <div className="my-6 w-full">
+          <p>Did you mean?</p>
+          <div className="my-2 grid w-full grid-cols-3 gap-4">{tagNodes}</div>
+        </div>
+      )}
+
+      <div className="mt-6 md:mt-12">
+        <p>
+          Showing {photoIDs.length} results for &quot;{query}&quot;
+        </p>
+        <PhotoGallery photoIDs={photoIDs} fadeIn />
+      </div>
     </PageWrapper>
   );
 };
