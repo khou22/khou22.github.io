61a61972300624f81a1ba7f800d8e154d3568a83
Kevin Hou <kevin.ch.hou@gmail.com>
2025-07-06 18:36:03 +0200

    Add photo title

--- src/app/photography/photo/[photo_id]/socials/PanoramaCarousel.tsx
+++ src/app/photography/photo/[photo_id]/socials/PanoramaCarousel.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { getCdnAsset, PhotoIdType } from "@/utils/cdn/cdnAssets";
+import { Aboreto } from "next/font/google";
+import { getCdnAsset, getPhotoName, PhotoIdType } from "@/utils/cdn/cdnAssets";
 import { getPhotoSize } from "@/utils/photos/getPhotoSize";
 import { INSTAGRAM_CAROUSEL_SIZE } from "@/constants/contentMetadata";
 import { classNames } from "@/utils/style";
+
+const captionFont = Aboreto({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 type PanoramaCarouselProps = {
   photoID: PhotoIdType;
@@ -12,6 +18,7 @@ export const PanoramaCarousel: React.FC<PanoramaCarouselProps> = async ({
   photoID,
 }) => {
   const size = await getPhotoSize(photoID);
+  const photoName = await getPhotoName(photoID);
 
   if (!size.width || !size.height) return <p>Error: No size for photo</p>;
 
@@ -24,7 +31,7 @@ export const PanoramaCarousel: React.FC<PanoramaCarouselProps> = async ({
   const imagePreviewHeight = "md:h-[400px] h-[300px]";
 
   return (
-    <div>
+    <div className="w-full space-y-4">
       <p>Num images: {normalizedNumCarouselImages.toFixed(2)}</p>
 
       <div
@@ -71,6 +78,45 @@ export const PanoramaCarousel: React.FC<PanoramaCarouselProps> = async ({
             );
           })}
         </div>
+      </div>
+
+      <div
+        className={classNames(
+          imagePreviewHeight,
+          "relative overflow-scroll bg-white",
+        )}
+        style={{
+          aspectRatio:
+            INSTAGRAM_CAROUSEL_SIZE.width / INSTAGRAM_CAROUSEL_SIZE.height,
+        }}
+      >
+        <div
+          className={classNames(
+            "absolute left-0 top-0 z-10 flex h-full w-full",
+            "flex-col items-center justify-center space-y-2 p-2",
+          )}
+        >
+          <img
+            alt="preview image full"
+            src={getCdnAsset(photoID)}
+            className="w-full"
+          />
+
+          <p
+            className={classNames(
+              "w-full text-center text-sm",
+              captionFont.className,
+            )}
+          >
+            {photoName.toUpperCase()}
+          </p>
+        </div>
+
+        <img
+          alt="preview image full background"
+          src={getCdnAsset(photoID)}
+          className="absolute left-1/2 top-1/2 z-0 h-full -translate-x-1/2 -translate-y-1/2 scale-110 object-cover opacity-80 blur-md"
+        />
       </div>
     </div>
   );
